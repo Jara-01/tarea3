b95d6c50d371d08b3b0ff7ed3a8c33f7da910a02
Benja <benjaminrrivassepul@gmail.com>
2026-06-01 23:50:15 -0400

    Cambia logica de dibujado de la expendedora

--- src/main/java/UserInterface/PanelExpendedor.java
+++ src/main/java/UserInterface/PanelExpendedor.java
@@ -6,8 +6,10 @@ import java.awt.*;
 import Logica.Expendedor;
 
 public class PanelExpendedor extends JPanel {
+    final int coordX = 50;
+    final private int coordY = 50;
+
     private Expendedor expendedor;
-    private Image base;
 
     private PanelDeposito coca;
     private PanelDeposito sprite;
@@ -19,26 +21,44 @@ public class PanelExpendedor extends JPanel {
     private PanelProducto productoFinal;
 
     public PanelExpendedor(){
+        setBackground(new Color(222, 228, 234));
+
         expendedor = new Expendedor(5);
-        base = new ImageIcon(getClass().getResource("/expendedora.png")).getImage();
 
-        coca     = new PanelDeposito(null,100,120);
-        sprite   = new PanelDeposito(null,100,220);
-        fanta    = new PanelDeposito(null,100,320);
-        snickers = new PanelDeposito(null,100,420);
-        super8   = new PanelDeposito(null,100,520);
+        coca     = new PanelDeposito(null, coordX + 40,coordY + 40);
+        sprite   = new PanelDeposito(null, coordX + 40,coordY + 140);
+        fanta    = new PanelDeposito(null, coordX + 40,coordY + 240);
+        snickers = new PanelDeposito(null, coordX + 40,coordY + 340);
+        super8   = new PanelDeposito(null, coordX + 40,coordY + 440);
 
-        monVu = new PanelDeposito(null,500,500);
-        productoFinal = new PanelProducto(252,655);
+        monVu = new PanelDeposito(null,coordX + 430,coordY + 405);
+        //productoFinal = new PanelProducto(coordX + 202,coordY + 605, null);
     }
 
     @Override
     public void paintComponent (Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.BLACK);
-        g.fillRect(50,50,300,400);
-        g.drawImage(base, 50, 50, 600, 700, null);
+        //fondo rojo
+        g.setColor(new Color(16, 122, 133));
+        g.fillRect(coordX,coordY,600,700);
+
+        g.setColor(Color.black);
+        //fonfo de contenedores
+        g.fillRect(coordX + 25,coordY + 25,380,515);
+        //espacio de producto comprado
+        g.fillRect(coordX + 140,coordY + 570,140,100);
+
+        //contenedores
+        g.setColor(Color.darkGray);
+        g.fillRect(coordX + 40, coordY + 40, 350, 85);
+        g.fillRect(coordX + 40, coordY + 140, 350, 85);
+        g.fillRect(coordX + 40, coordY + 240, 350, 85);
+        g.fillRect(coordX + 40, coordY + 340, 350, 85);
+        g.fillRect(coordX + 40, coordY + 440, 350, 85);
+
+        //zona vuelto
+        g.fillRect(coordX + 425, coordY + 405, 150, 100);
 
         coca.paintComponent(g);
         sprite.paintComponent(g);
@@ -46,6 +66,6 @@ public class PanelExpendedor extends JPanel {
         snickers.paintComponent(g);
         super8.paintComponent(g);
         monVu.paintComponent(g);
-        productoFinal.paintComponent(g);
+        //productoFinal.paintComponent(g);
     }
 }
